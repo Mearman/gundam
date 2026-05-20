2ca9504a90767dc5ecdea3b5c5bfb1fb4948a784
Joseph Mearman <joseph@mearman.co.uk>
2026-05-20 22:43:43 +0100

    refactor: rename y1/y2 to yearStart/yearEnd in stacking algorithm

--- src/data/stacking.ts
+++ src/data/stacking.ts
@@ -8,21 +8,23 @@ import type { Entry, StackedEntry } from "../data/types";
  */
 export function assignStacks(entries: Entry[]): StackedEntry[] {
   const sorted = [...entries].sort(
-    (a, b) => a.y1 - b.y1 || b.y2 - b.y1 - (a.y2 - a.y1),
+    (a, b) =>
+      a.yearStart - b.yearStart ||
+      b.yearEnd - b.yearStart - (a.yearEnd - a.yearStart),
   );
   const stacks: number[] = []; // stacks[i] = max end year used in row i
 
   return sorted.map((e) => {
     let assigned = -1;
     for (let i = 0; i < stacks.length; i++) {
-      if (stacks[i] < e.y1) {
-        stacks[i] = e.y2;
+      if (stacks[i] < e.yearStart) {
+        stacks[i] = e.yearEnd;
         assigned = i;
         break;
       }
     }
     if (assigned === -1) {
-      stacks.push(e.y2);
+      stacks.push(e.yearEnd);
       assigned = stacks.length - 1;
     }
     return { ...e, stack: assigned };
